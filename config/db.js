37e25d2630309f8277426304d794913ac0cd6b5a
unknown <you@example.com>
2021-12-06 12:34:42 +0200

    some msg

--- config/db.js
+++ config/db.js
@@ -5,8 +5,6 @@ const connectDB = async () => {
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
-			useCreateIndex: true,
-			allow_embedding:true
 		});
 		console.log(`MongoDB connected ${conn.connection.host}`);
 	} catch (error) {
