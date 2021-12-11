70ad28ef1c4bd7610801960afc7fe6c0bb2e034c
unknown <you@example.com>
2021-12-12 00:47:54 +0200

    some msg

--- routes/recipe.route.js
+++ routes/recipe.route.js
@@ -3,10 +3,10 @@ const recipeController = require("../controllers/recipe.controller");
 const router = express.Router();
 
 
-router.get("/", (req, res) => {
+router.get("/recipe", (req, res) => {
   recipeController.getAllrecipes(req, res);
 })
-router.post("/", (req, res) => {
+router.post("/recipe", (req, res) => {
     recipeController.addNewrecipes(req, res);
   })
   router.put("/:id", (req, res) => {
